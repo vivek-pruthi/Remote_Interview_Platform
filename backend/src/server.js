c1f0d9628a464b63ac58ce8cf4730d8f4f57acd6
vivek_pruthi <vivek1787.becse24@chitkara.edu.in>
2026-02-10 22:53:02 +0530

    fix vite build

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ if (ENV.NODE_ENV === 'production') {
 
     app.use(express.static(frontendPath));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(frontendPath, 'index.html'));
     });
 }
