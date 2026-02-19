d24f5113731d8e93013522698cca6d7fb9ddd2fe
vivek_pruthi <vivek1787.becse24@chitkara.edu.in>
2026-02-19 14:32:29 +0530

    modiefied

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { ConnectDB } from "./db.js";
-import { User } from "./models/User.js";
+import { User } from "../models/User.js";
 
 
 export const inngest = new Inngest({ id: "CodeBridge" });
