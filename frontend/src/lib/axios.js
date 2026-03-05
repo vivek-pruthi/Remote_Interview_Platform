460e297576ffd32011f9e50a282a96826ddecd73
vivek_pruthi <vivek1787.becse24@chitkara.edu.in>
2026-03-05 20:53:26 +0530

    Fix production API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from"axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, //base url for all the requests
+    baseURL: import.meta.env.VITE_API_URL|| "/api", //base url for all the requests
     withCredentials: true //by adding this field browser will send cookies to server automatically, on every single request
 })
 
