700d33900586a342a1c5c4fb2fec22f3b7dc1b7a
vivek_pruthi <vivek1787.becse24@chitkara.edu.in>
2026-03-10 14:35:37 +0530

    Fix participant field mismatch

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -1,37 +1,42 @@
 import mongoose from "mongoose";
 
-const SessionSchema = new mongoose.Schema({
-    problem: {
-        type:String,
-        required: true,
-    },
-    difficulty: {
-        type: String,
-        enum: ["easy", "medium", "hard"],
-        required: true,
-    },
-    host: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    participants: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        default: null,
-    },
-    status: {
-        type: String,
-        enum: ["active", "completed"],
-        default: "active",
-    },
-    // stream video call Id
-    callId : {
-        type: String,
-        default: "",
-    }
+const SessionSchema = new mongoose.Schema(
+{
+  problem: {
+    type: String,
+    required: true,
+  },
+
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: true,
+  },
+
+  host: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  participant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+
+  status: {
+    type: String,
+    enum: ["active", "completed"],
+    default: "active",
+  },
+
+  callId: {
+    type: String,
+    default: "",
+  },
 },
-    {timestamps: true,}
+{ timestamps: true }
 );
 
 const Session = mongoose.model("Session", SessionSchema);
